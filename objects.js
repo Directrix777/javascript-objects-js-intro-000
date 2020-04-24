4947e42a4cf05fa2bdb77bfe7f243d805006ba63
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 19:44:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {IdinaMenzel: `Let It Go`, Incubus: `Drive`}
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  playlist.artistname = songTitle
+  playlist.[artistName] = songTitle
 }
